02be8bdbd5a477f6769f37d664baccdcce9add30
Clark Boylan <clark.boylan@gmail.com>
2010-04-02 23:43:21 -0700

    remove dead code/comment from dzen_batt.sh

--- bin/dzen_batt.sh
+++ bin/dzen_batt.sh
@@ -7,5 +7,4 @@ YPOS=10
 BG="Black"
 FG="White"
 
-#acpi -i | sed -n 's/^Battery 0: \(D\?i\?s\?[cC]harging, [0-9]\{1,2\}\%, [0-9]\{2\}:[0-9]\{2\}:[0-9]\{2\} \(until charged\|remaining\)$\)/\1/p' | dzen2 -p $TICKS -w $WIDTH -bg $BG -fg $FG -x $XPOS -y $YPOS
 acpi | sed -n 's/^Battery 0: \(.*\)$/\1/p' | dzen2 -p $TICKS -w $WIDTH -bg $BG -fg $FG -x $XPOS -y $YPOS
